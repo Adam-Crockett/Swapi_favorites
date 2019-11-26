8234416c9c098295bc698ba783cec8b37cedfec2
Adam Crockett <arc1224@gmail.com>
2019-11-26 12:45:43 -0800

    Got reset button working

--- barsorting/urls.py
+++ barsorting/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sorting, name='sorting'),
+    path('', views.reset_bars, name='sorting'),
+    path('reset', views.reset_bars, name='sorting')
 ]
--- barsorting/views.py
+++ barsorting/views.py
@@ -6,7 +6,14 @@ from .models import BarValues
 # Create your views here.
 
 
-def sorting(request):
+# def sorting_page(request):
+#     bar_set = BarValues()
+#     context = {
+#         'bar_set': bar_set.get_bar_values,
+#     }
+#     return render(request, 'barsorting/sorting.html', context)
+
+def reset_bars(request):
     bar_set = BarValues()
     context = {
         'bar_set': bar_set.get_bar_values,
