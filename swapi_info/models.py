5c0c19567be1d78c2d5cdc297f99a928cdba1d08
Adam Crockett <arc1224@gmail.com>
2020-01-12 21:05:18 -0800

    Created Model for favorites database

--- swapi_info/models.py
+++ swapi_info/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Model Structure
 # The model should hold the favorites in the db
-#
+
+
+class Favorites(models.Model):
+    name = models.CharField(max_length=30)
+    item_type = models.CharField(max_length=10)
+    swapi_url = models.CharField(max_length=50)
+    favorite_count = models.IntegerField()
