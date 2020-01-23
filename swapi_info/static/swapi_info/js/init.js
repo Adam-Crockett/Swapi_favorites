b83ff70405534d5d7130a5225add3d7f87ef4c0e
Adam Crockett <arc1224@gmail.com>
2020-01-22 23:26:04 -0800

    Updated all details pages

--- swapi_info/static/swapi_info/js/init.js
+++ swapi_info/static/swapi_info/js/init.js
@@ -1,9 +1,18 @@
 (function($) {
   $(function() {
     $(".sidenav").sidenav();
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems, options);
   }); // end of document ready
 });
+
 $(document).ready(function() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems, options);
+  });
   $("select").formSelect();
 });
 jQuery; // end of jQuery name space
