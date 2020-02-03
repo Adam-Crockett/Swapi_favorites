1dd13060924d80036f9cc1f9a041fa5ab6efac41
Adam Crockett <arc1224@gmail.com>
2020-02-02 18:17:55 -0800

    config changes

--- DjangoPortfolio/settings.py
+++ DjangoPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['swapi-info.xdmdgepvjs.us-west-1.elasticbeanstalk.com']
 
 
 # Application definition
